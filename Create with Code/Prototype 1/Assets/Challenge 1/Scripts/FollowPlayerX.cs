cf3d943fa74d7686544e9810fc7ee32536ee3853
Cernell Bash <blashout@gmail.com>
2024-04-19 18:39:58 -0700

    Solved most functionality, comments pending

--- Create with Code/Prototype 1/Assets/Challenge 1/Scripts/FollowPlayerX.cs
+++ Create with Code/Prototype 1/Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -1,16 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class FollowPlayerX : MonoBehaviour
 {
     public GameObject plane;
-    private Vector3 offset;
+    private Vector3 offset = new Vector3 (18, 0, 0);
 
     // Start is called before the first frame update
     void Start()
     {
-
+        //transform.rotation = plane.transform.rotation * Quaternion.Euler(0, -90, 0);
+        transform.Rotate(Vector3.up * -90);
     }
 
     // Update is called once per frame
